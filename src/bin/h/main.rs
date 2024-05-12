7cae899c781abb170bdd89c4c5c7a717c997aee4
Joshua Toyota <jearsh@gmail.com>
2024-05-12 15:17:43 -0400

    notes for how to handle control flow in main

--- src/bin/h/main.rs
+++ src/bin/h/main.rs
@@ -16,6 +16,23 @@ fn main() {
     let parsed_args = main_command().get_matches();
 
     if let Some((subcmd, _matches)) = parsed_args.subcommand() {
+
+
+        /*
+        * this could be written a few ways:
+        *
+        * - write a fn to return an enum of types like
+        *   - Internal(runner)
+        *   - External(bin_path)
+        * - write the external fn that takes an option
+        *   - return the arg if Some(_)
+        *   - look for external subcmd if None
+        * - use the Option methods like .value_or(find_external...)
+        *   to do the chaining for me
+        */
+
+
+
         //Some((subcmd, _matches)) => {
             // TODO: look for internally defined subcmd
 
